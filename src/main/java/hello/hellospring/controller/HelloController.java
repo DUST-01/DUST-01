6e405620403a3734a9951d198d392081f1488da4
ghchoi0427 <ghchoi0427@gmail.com>
2022-05-25 22:26:26 +0900

    feat: RequestParam으로 값 전달

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -12,5 +14,12 @@ public class HelloController {
         model.addAttribute("data", "Spring!!");
         return "hello";
     }
+
+    @GetMapping("hello-spring")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
 
 }
